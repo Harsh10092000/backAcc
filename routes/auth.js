354ec25f1ab879e72d70161edbc071f2063517dd
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-01-10 15:30:40 +0530

    commit

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendOtp, login } from "../controllers/auth.js";
-
-const router = express.Router();
-
-router.get("/sendmail/:email", sendOtp);
-router.post("/checklog", login);
-
-export default router;
