aa4677f157875c0496f799347799809f989bad28
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-02-22 13:27:51 +0530

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -1,12 +1,12 @@
 // import nodemailer from "nodemailer";
 
 // export const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
+//   host: "smtp.zeptomail.in",
 //   port: 587,
 //   secure: false,
 //   auth: {
-//     user: "accbook.calinfo007@gmail.com",
-//     pass: "zxqrpbamotuwvmct",
+//     user: "emailapikey",
+//     pass: "PHtE6r1cQLrigzIv80MD4/7qQM6nN4l9rOtuJFMTtdxDD/QEGE1RotAilmfj/08uUvRLQfLIyow64rub4rmCdzy/MjlKWmqyqK3sx/VYSPOZsbq6x00auFQbc03YUo/se9Ni1C3fvtrTNA==",
 //   },
 // });
 
