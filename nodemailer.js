44101281c4c1d589543bf647e9d41ea190fb3ac2
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-02-22 13:32:24 +0530

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -13,11 +13,11 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.hostinger.com",
+  host: "smtp.zeptomail.in",
   port: 587,
   secure: false,
   auth: {
-    user: "noreply@khataease.com",
-    pass: "Khataease@9729",
+    user: "emailapikey",
+    pass: "PHtE6r1cQLrigzIv80MD4/7qQM6nN4l9rOtuJFMTtdxDD/QEGE1RotAilmfj/08uUvRLQfLIyow64rub4rmCdzy/MjlKWmqyqK3sx/VYSPOZsbq6x00auFQbc03YUo/se9Ni1C3fvtrTNA==",
   },
 });
